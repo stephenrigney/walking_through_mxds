7f1a608fbfdc69c5c9531d6bf18ed6710f28b2de
stephenrigney <stephenrigney@users.noreply.github.com>
2020-06-12 15:24:18 +0100

    Proper syntax for a function call

--- all_vectors_rasters.py
+++ all_vectors_rasters.py
@@ -5,11 +5,30 @@ arcpy.env.workspace = "C:/Projects/AIRO/LARES/GeoDatabases/2. Renewable Energy P
 output_workspace = "C:/Projects/AIRO/LARES/GeoDatabases/outputs"
 
 #getting a syntax error here. I know why because the use of def is not right, I know this.
-def fields(arcpy.ListFields("C:/Projects/AIRO/LARES/GeoDatabases/2. Renewable Energy Planning/"))
-for field in fields:
+#def fields(arcpy.ListFields("C:/Projects/AIRO/LARES/GeoDatabases/2. Renewable Energy Planning/"))
+#for field in fields:
+#         print("{0} is a type of {1} with a length of {2}"
+#         .format(field.name, field.type, field.length))
+'''
+Syntax for a function is:
+
+def a_function(variable):
+    # Notice that the code within the function is indented
+    output = do something with variable
+    return output 
+'''
+def print_field_info(filepath)
+    fields = arcpy.ListFields(filepath)
+    for field in fields:
          print("{0} is a type of {1} with a length of {2}"
          .format(field.name, field.type, field.length))
+    return fields # if you need to use function output
 
+# Declare variable
+fp = "C:/Projects/AIRO/LARES/GeoDatabases/2. Renewable Energy Planning/"
+
+# Call function with fp as variable for filepath
+print_field_info(fp)
 
 workspace = "C:/Projects/AIRO/LARES/GeoDatabases/2. Renewable Energy Planning/"
 feature_classes = []
