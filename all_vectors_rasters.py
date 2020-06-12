928d6e7e279c4c2677cae96b7e6616827c90fa59
Eoin O'Mahony <eoin.omahony@ucd.ie>
2020-06-12 15:06:25 +0100

    Update all_vectors_rasters.py

--- all_vectors_rasters.py
+++ all_vectors_rasters.py
@@ -4,6 +4,7 @@ import os
 arcpy.env.workspace = "C:/Projects/AIRO/LARES/GeoDatabases/2. Renewable Energy Planning/"
 output_workspace = "C:/Projects/AIRO/LARES/GeoDatabases/outputs"
 
+#getting a syntax error here. I know why because the use of def is not right, I know this.
 def fields(arcpy.ListFields("C:/Projects/AIRO/LARES/GeoDatabases/2. Renewable Energy Planning/"))
 for field in fields:
          print("{0} is a type of {1} with a length of {2}"
